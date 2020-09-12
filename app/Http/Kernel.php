11050e7d5ba02a0a6b2cc4efe79ca878f74f4e3f
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-12 23:39:02 +0200

    Remove PreventRequestsDuringMaintenance::class

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -7,7 +7,6 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 class Kernel extends HttpKernel
 {
     protected $middleware = [
-        \Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \Illuminate\Foundation\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
