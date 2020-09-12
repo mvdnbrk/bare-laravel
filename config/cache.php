dfd647075dfff0f401a46a2a70e5a296f8310d8f
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-12 22:45:48 +0200

    Remove `memcached` cache store

--- config/cache.php
+++ config/cache.php
@@ -24,25 +24,6 @@ return [
             'path' => storage_path('framework/cache/data'),
         ],
 
-        'memcached' => [
-            'driver' => 'memcached',
-            'persistent_id' => env('MEMCACHED_PERSISTENT_ID'),
-            'sasl' => [
-                env('MEMCACHED_USERNAME'),
-                env('MEMCACHED_PASSWORD'),
-            ],
-            'options' => [
-                // Memcached::OPT_CONNECT_TIMEOUT => 2000,
-            ],
-            'servers' => [
-                [
-                    'host' => env('MEMCACHED_HOST', '127.0.0.1'),
-                    'port' => env('MEMCACHED_PORT', 11211),
-                    'weight' => 100,
-                ],
-            ],
-        ],
-
         'dynamodb' => [
             'driver' => 'dynamodb',
             'key' => env('AWS_ACCESS_KEY_ID'),
