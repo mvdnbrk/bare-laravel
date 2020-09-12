a6860dd11a9d58ba13b26c5c23ec3060aca8a37d
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-12 21:16:09 +0200

    Remove rate limiting configuration

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,17 +2,13 @@
 
 namespace App\Providers;
 
-use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->configureRateLimiting();
-
         $this->routes(function () {
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
@@ -21,10 +17,5 @@ class RouteServiceProvider extends ServiceProvider
                 ->middleware('api')
                 ->group(base_path('routes/api.php'));
         });
-    }
-
-    protected function configureRateLimiting(): void
-    {
-        RateLimiter::for('api', fn () => Limit::perMinute(60));
     }
 }
