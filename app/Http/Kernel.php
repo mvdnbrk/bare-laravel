43edb16e0b558098f87e46b0a5e5123b6c37347d
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-12 23:07:35 +0200

    Remove fideloper/proxy

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -7,7 +7,6 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 class Kernel extends HttpKernel
 {
     protected $middleware = [
-        \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \Illuminate\Foundation\Http\Middleware\TrimStrings::class,
