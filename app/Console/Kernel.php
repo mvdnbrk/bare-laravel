ee7a22473c047466899cfde8676bec02c726e467
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-12 19:03:41 +0200

    Remove command method call `inspire`

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -21,7 +21,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        //
     }
 
     /**
