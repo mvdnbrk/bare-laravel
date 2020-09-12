c66dfa25c5725cb2c7ebd411e7780e797a32a893
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-12 22:47:41 +0200

    Remove `file` cache store

--- config/cache.php
+++ config/cache.php
@@ -13,11 +13,6 @@ return [
             'serialize' => false,
         ],
 
-        'file' => [
-            'driver' => 'file',
-            'path' => storage_path('framework/cache/data'),
-        ],
-
     ],
 
     'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_cache'),
