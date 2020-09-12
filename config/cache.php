1d3a296c7f61f5a7532d978963786bd2135663e7
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-12 22:46:09 +0200

    Remove `dynamodb` cache store

--- config/cache.php
+++ config/cache.php
@@ -24,15 +24,6 @@ return [
             'path' => storage_path('framework/cache/data'),
         ],
 
-        'dynamodb' => [
-            'driver' => 'dynamodb',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-            'table' => env('DYNAMODB_CACHE_TABLE', 'cache'),
-            'endpoint' => env('DYNAMODB_ENDPOINT'),
-        ],
-
     ],
 
     'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_cache'),
