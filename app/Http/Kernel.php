520338e571669dd91f3c3efc63a0dce2256ed089
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-12 21:34:06 +0200

    Remove commented out AuthenticateSession::class

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,7 +20,6 @@ class Kernel extends HttpKernel
             \Illuminate\Cookie\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
-            // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
