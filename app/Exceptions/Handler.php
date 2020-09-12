7723e74f7acb0ba687a5dcf056067b079ef56ab7
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-12 17:58:22 +0200

    Add return type declarations

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -27,10 +27,8 @@ class Handler extends ExceptionHandler
 
     /**
      * Register the exception handling callbacks for the application.
-     *
-     * @return void
      */
-    public function register()
+    public function register(): void
     {
         //
     }
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -30,10 +30,8 @@ class RouteServiceProvider extends ServiceProvider
 
     /**
      * Define your route model bindings, pattern filters, etc.
-     *
-     * @return void
      */
-    public function boot()
+    public function boot(): void
     {
         $this->configureRateLimiting();
 
@@ -49,10 +47,8 @@ class RouteServiceProvider extends ServiceProvider
 
     /**
      * Configure the rate limiters for the application.
-     *
-     * @return void
      */
-    protected function configureRateLimiting()
+    protected function configureRateLimiting(): void
     {
         RateLimiter::for('api', fn(Request $request) => Limit::perMinute(60));
     }
