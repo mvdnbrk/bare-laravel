b034e8050de9b96f4c96f81dec509746788d516b
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-12 20:21:39 +0200

    Typehint Request in Authenticate class

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -9,10 +9,8 @@ class Authenticate extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
      */
-    protected function redirectTo($request): ?string
+    protected function redirectTo(Request $request): ?string
     {
         if (! $request->expectsJson()) {
             return route('login');
