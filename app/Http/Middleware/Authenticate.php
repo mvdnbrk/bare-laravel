8616784511e63cd16558bfaf4206f7ce98257acd
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-12 21:10:40 +0200

    Remove default redirectTo method from Authenticate middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,13 +2,9 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Http\Request;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
-    protected function redirectTo(Request $request): ?string
-    {
-        //
-    }
+    //
 }
