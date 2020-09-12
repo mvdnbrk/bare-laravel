6dcbbda6b1eb8657426acef6d7fd59fd5e1a2ac9
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-12 20:39:23 +0200

    Remove comment in Exceptions/Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -6,9 +6,6 @@ use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * Register the exception handling callbacks for the application.
-     */
     public function register(): void
     {
         //
