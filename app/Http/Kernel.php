2c458155bc36966e57a77b7db0f5653bce496203
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-13 15:08:13 +0200

    Remove $middleware from Http/Kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -6,12 +6,6 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
 {
-    protected $middleware = [
-        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
-        \Illuminate\Foundation\Http\Middleware\TrimStrings::class,
-        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-    ];
-
     protected $middlewareGroups = [
         'web' => [
             \Illuminate\Cookie\Middleware\EncryptCookies::class,
