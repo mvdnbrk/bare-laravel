edfd6648fe358e9fd3a109cdac414b6e2eef328f
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-13 15:25:44 +0200

    Remove Http/Kernel.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,7 +6,7 @@ $app = new Illuminate\Foundation\Application(
 
 $app->singleton(
     Illuminate\Contracts\Http\Kernel::class,
-    App\Http\Kernel::class
+    Illuminate\Foundation\Http\Kernel::class,
 );
 
 $app->singleton(
