135ced6cf178fbdd38287731e53bacaae09edbe8
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-13 15:14:23 +0200

    Remove VerifyCsrfToken middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -12,7 +12,6 @@ class Kernel extends HttpKernel
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
         ],
     ];
 }
